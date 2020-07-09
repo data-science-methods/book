a6f545c32d818c9322151b17a646f78242ad9475
Dan Hicks <hicks.daniel.j@gmail.com>
2020-07-09 16:16:46 -0700

    fix quote in deploy

--- _deploy.sh
+++ _deploy.sh
@@ -3,14 +3,14 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 1
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "hicks.daniel.j@gmail.com""
+git config --global user.email "hicks.daniel.j@gmail.com"
 git config --global user.name "Dan Hicks"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../docs/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Update the book" || true
 git push -q origin gh-pages
